dd9eb10fd86a9134b06434ce08be113a3a9017f3
Strange <nazaroff.serezha2014@gmail.com>
2021-04-09 15:45:35 +0300

    New Trace

---
+++ Tests/TestAuto/TestAuto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Transport_company;
+using System.Xml.Serialization;
+
+namespace TestAuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            auto Basa = new auto(); XmlSerializer formatter = new XmlSerializer(typeof(auto));
+            string Auto, Number, Rider, Sr = "";
+            Console.WriteLine("ВВедите поочередно название авто, номер авто и имя водителя");
+            Auto = Console.ReadLine();
+            Number = Console.ReadLine();
+            Rider = Console.ReadLine();
+            Basa.Add(Auto, Number, Rider);
+            Console.WriteLine("Введите о чем хотите вывести информацию");
+            Sr = Console.ReadLine();
+            Console.WriteLine(Basa.SearchAndPrint(Sr));
+
+        }
+    }
+}
--- Transport company/Transport company/trace.cs
+++ Transport company/Transport company/trace.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,134 @@ using System.Threading.Tasks;
 
 namespace Transport_company
 {
-    class trace
+    class Trace<T> 
     {
+        DoublyNode<T> Head;
+        DoublyNode<T> Tail;
+        int Count;
+        public void Add(T Start, T Finish, DateTime dateTime)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(Start, Finish, dateTime);
+
+            if (Head == null)
+            {
+                Head = node;
+                Head.Next = node;
+                Head.Previous = node;
+            }
+            else
+            {
+                node.Previous = Head.Previous;
+                node.Next = Head;
+                Head.Previous.Next = node;
+                Head.Previous = node;
+            }
+            Tail = node;
+            Count++;
+        }
+        public bool Remove(String start, string finish)
+        {
+            DoublyNode<T> current = Head;
+
+            DoublyNode<T> removedItem = null;
+            if (Count == 0) return false;
+
+            // поиск удаляемого узла
+            do
+            {
+                if (current.Start1.Equals(start) && current.Finish1.Equals(finish))
+                {
+                    removedItem = current;
+                    break;
+                }
+                current = current.Next;
+            }
+            while (current != Head);
+
+            if (removedItem != null)
+            {
+                // если удаляется единственный элемент списка
+                if (Count == 1)
+                    Head = null;
+                else
+                {
+                    // если удаляется первый элемент
+                    if (removedItem == Head)
+                    {
+                        Head = Head.Next;
+                    }
+                    removedItem.Previous.Next = removedItem.Next;
+                    removedItem.Next.Previous = removedItem.Previous;
+                }
+                Count--;
+                return true;
+            }
+            return false;
+        }
+
+        //IEnumerator<string> IEnumerable<T>.GetEnumerator()
+        //{
+        //        DoublyNode<T> current = Head;
+        //            do
+        //            {
+        //                string Sim = "Start: " + current.Start1 + " " + "Finish: " + current.Finish1 + " " + "Time: " + current.Time1;
+        //                if (current != null)
+        //                {
+        //                    yield return Sim;
+        //                    current = current.Next;
+        //                }
+        //}
+        //while (current != Head) ;
+        //}
+
+        public String PrintAll()
+        {
+            string Sim = "";
+            DoublyNode<T> current = Head;
+            do
+            {
+                Sim = Sim + "Start: " + current.Start1 + " " + "Finish: " + current.Finish1 + " " + "Time: " + current.Time1 + "\n";
+                if (current != null)
+                {
+                    current = current.Next;
+                }
+            }
+            while (current != Head);
+            return Sim;
+        }
+        //public string PrintAndSearch(T Input)
+        //{
+        //    string Sim = "";
+        //    DoublyNode<T> current = Head;
+        //    do
+        //    {
+        //        if (current != null)
+        //        {
+        //            current = current.Next;
+        //            if(current.Finish1 == Input || current.Start1 == Input)
+        //            {
+
+        //            }
+        //        }
+        //    }
+        //    while (current != Head);
+        //}
+    }
+    public class DoublyNode<T>
+    {
+        public DoublyNode(T start, T finish, DateTime dateTime)
+        {
+            Start1 = start;
+            Finish1 = finish;
+        }
+
+        private T Start;
+        private T Finish;
+        private DateTime Time;
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+        public T Start1 { get => Start; set => Start = value; }
+        public T Finish1 { get => Finish; set => Finish = value; }
+        public DateTime Time1 { get => Time; set => Time = value; }
     }
 }
